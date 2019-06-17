5cea11275da9cf69cd5cc83a82b8f624c0190050
FlameInTheDark <nin-vain@mail.ru>
2019-06-17 08:23:28 +0500

    Start working on sql protocols

--- connector/mysql/mysql.go
+++ connector/mysql/mysql.go
@@ -1,1 +1,15 @@
 package mysql
+
+type MysqlProtocol struct {
+	dst      string
+}
+
+func Create(address, port string) *MysqlProtocol {
+	var dst string
+	if port == "0" {
+		dst = address + ":3306"
+	} else {
+		dst = address + ":" + port
+	}
+	return &MysqlProtocol{dst}
+}
